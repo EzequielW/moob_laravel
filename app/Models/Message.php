c6f887e512d8685d94e7004c353bf72e6c878638
EzequielW <ezequielwokraczka@gmail.com>
2025-05-18 18:56:25 -0300

    Fix composer setup and message seeder

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['user_id', 'platform', 'recipient', 'content', 'attachment_path'];
 
     public function user()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => '12345678'
         ]);
 
-        Message::factory(30)->create();
+        Message::factory(30)->for($user)->create();
     }
 }
